69c6ff16ebec7d8f1d788dd99c70a483352911b1
nelkahn <nelkahn@gmail.com>
2016-02-22 14:51:00 +0100

    add NonNull

--- SharePlot/javasource/jet/shareplot/ui/desktop/SharePlotMenuConstants.java
+++ SharePlot/javasource/jet/shareplot/ui/desktop/SharePlotMenuConstants.java
@@ -1,16 +1,19 @@
 package jet.shareplot.ui.desktop;
 
+import org.eclipse.jdt.annotation.NonNull;
+
 /**
  * Menu constants.
- * 
+ *
  * @author daniel
- * 
+ *
  */
 public final class SharePlotMenuConstants {
 
     /**
      * <code>MAIN_MENU_BAR</code>.
      */
+    @NonNull
     public static final String MAIN_MENU_BAR = "SharePlotMainMenuBar";
     /**
      * <code>MAIN_MENU_BAR</code>.
